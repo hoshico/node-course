35fdf1355741c325e7a929a1243bdecd992b8c49
hoshico <melancholinista.yasu@gmail.com>
2022-08-18 20:18:56 +0900

    install validator

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,7 +1,13 @@
 //const add = require('./utils.js')
 //const sum = add(4, -2)
 //console.log(sum)
+const validator = require('validator')
+const meg = require('./notes.js')
 
-const message = require('./notes.js')
+console.log(meg());
 
-console.log(message());
+// emailかどうかのバリデーション(true/false)
+console.log(validator.isEmail('endrew@exaple.com'))
+
+// URLかどうかのバリデーション(true/false)
+console.log(validator.isURL('https://mead.io'))
