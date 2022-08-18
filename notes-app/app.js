d4a6849c749b8d22cca378135e91a15a398d1423
hoshico <melancholinista.yasu@gmail.com>
2022-08-18 16:04:57 +0900

    add appendfilesync to app.js

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,3 +1,4 @@
 const fs = require('fs')
 
-fs.writeFileSync('notes.txt', 'My nami is Andrew.')
+fs.writeFileSync('notes.txt', 'My nami is Andrew. ')
+fs.appendFileSync('notes.txt', ' I live in Philadelphia')
