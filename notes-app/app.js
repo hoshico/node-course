0c15c14aa0049874b4d959918ad2e7c3f7245dbd
hoshico <melancholinista.yasu@gmail.com>
2022-08-20 07:09:09 +0900

    add arg to app.js

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,8 +1,12 @@
 const chalk = require('chalk');
+const getNotes = require('./notes.js')
 
-const log = console.log;
-const meg = require('./notes.js')
+const command = process.argv[2]
 
-console.log(meg());
+console.log(process.argv)
 
-log(chalk.bold.green("Success"));
+if (command === 'add') {
+  console.log('adding note!')
+} else if (command === 'remove') {
+  console.log('removing note!')
+}
