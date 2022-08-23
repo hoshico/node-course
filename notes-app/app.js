132d791ad70c64073382be60ccc3a2889b2956d3
hoshico <melancholinista.yasu@gmail.com>
2022-08-24 08:13:33 +0900

    add yargs

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,12 +1,46 @@
 const chalk = require('chalk');
-const getNotes = require('./notes.js')
+const yargs = require('yargs');
+const getNotes = require('./notes.js');
 
-const command = process.argv[2]
+// Customize yargs version
+yargs.version('1.1.0')
 
-console.log(process.argv)
+// Create add command
+yargs.command({
+  command: 'add',
+  describe: 'Add a new note',
+  handler: function () {
+    console.log('Adding a new note!')
+  }
+})
 
-if (command === 'add') {
-  console.log('adding note!')
-} else if (command === 'remove') {
-  console.log('removing note!')
-}
+// Create remove command
+yargs.command({
+  command: 'remove',
+  describe: 'Remove a note',
+  handler: function () {
+    console.log('Removing the note')
+  }
+})
+
+// Create list command
+yargs.command({
+  command: 'list',
+  describe: 'List your notes',
+  handler: function () {
+    console.log('Listing out all notes')
+  }
+})
+
+// Create read command
+yargs.command({
+  command: 'read',
+  describe: 'Read a notes',
+  handler: function () {
+    console.log('reading a notes')
+  }
+})
+
+// add, remove, read, list
+
+console.log(yargs.argv);
