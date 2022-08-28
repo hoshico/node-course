21f927882aa15a8ae925479294bd5e23879fc5ba
hoshico <melancholinista.yasu@gmail.com>
2022-08-28 14:50:12 +0900

    add saveNotes

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,7 +1,6 @@
 const chalk = require('chalk');
-const { describe } = require('yargs');
 const yargs = require('yargs');
-const getNotes = require('./notes.js');
+const notes = require('./notes.js');
 
 // Customize yargs version
 yargs.version('1.1.0')
@@ -23,8 +22,7 @@ yargs.command({
     }
   },
   handler: function (argv) {
-    console.log('title: ' + argv.title );
-    console.log('body: ' + argv.body );
+    notes.addNote(argv.title, argv.body);
   }
 })
 
