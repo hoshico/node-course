84975bfeb99851f5fe05c9c10b2db536b738ea32
hoshico <melancholinista.yasu@gmail.com>
2022-09-25 19:29:07 +0900

    add json: true

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,8 +1,8 @@
 const request = require('request')
 
-const url = 'http://api.weatherstack.com/current?access_key=30bafc6745bcf550e2ce0d458dd85abd&query=37.8267,-122.4233'
+const url = 'http://api.weatherstack.com/current?access_key=30bafc6745bcf550e2ce0d458dd85abd&query=37.8267,-122.4233&units=f'
 
-request({ url: url }, (error, response) => {
-  const data = JSON.parse(response.body)
-  console.log(data)
+request({ url: url, json: true }, (error, response) => {
+  //console.log(response.body.current)
+  console.log(`${response.body.current.weather_descriptions[0]}. It is curently ${response.body.current.temperature} degrees out. There is a ${response.body.current.precip} % chance of rain.`)
 })
